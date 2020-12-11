98bf5a5334d57e5063cda700e1002753bfeaf0c9
Tobias <tobber00@gmail.com>
2020-12-11 15:05:42 +0100

    what

--- Room_related/Trash.java
+++ Room_related/Trash.java
@@ -2,19 +2,18 @@ package Room_related;
 
 public class Trash {
     // Attributter:
-    /*private String material;*/
     private String name;
     private TrashType trashType;
-    /*private int x;
-    private int y;*/
+    private int x;
+    private int y;
 
     // Constructor: objects (trash) are made here
-    public Trash (/*String material,*/ String name, TrashType trashType/*, int x, int y*/){
+    public Trash (/*String material,*/ String name, TrashType trashType, int x, int y){
         /*this.material = material;*/
         this.name = name;
         this.trashType = trashType;
-        /*this.x = x;
-        this.y = y;*/
+        this.x = x;
+        this.y = y;
         // without constructor here we can't create trash objects in Room
     }
 
